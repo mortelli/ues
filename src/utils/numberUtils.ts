8c923c64cae96e58a27eb95a644b8bfadadbd51f
mortelli <ortellimarcelo@gmail.com>
2025-01-23 17:59:14 -0300

    rename number literals to spanish

--- src/utils/numberUtils.ts
+++ src/utils/numberUtils.ts
@@ -3,40 +3,40 @@ export function translateNumber(inputNumber: number): string {
   // Arrays to hold words for single-digit, double-digit, and below-hundred numbers
   const single_digit = [
     '',
-    'One',
-    'Two',
-    'Three',
-    'Four',
-    'Five',
-    'Six',
-    'Seven',
-    'Eight',
-    'Nine',
+    'Uno',
+    'Dos',
+    'Tres',
+    'Cuatro',
+    'Cinco',
+    'Seis',
+    'Siete',
+    'Ocho',
+    'Nueve',
   ];
   const double_digit = [
-    'Ten',
-    'Eleven',
-    'Twelve',
-    'Thirteen',
-    'Fourteen',
-    'Fifteen',
-    'Sixteen',
-    'Seventeen',
-    'Eighteen',
-    'Nineteen',
+    'Diez',
+    'Once',
+    'Doce',
+    'Trece',
+    'Catorce',
+    'Quince',
+    'Dieciseis',
+    'Diecisiete',
+    'Dieciocho',
+    'Diecinueve',
   ];
   const below_hundred = [
-    'Twenty',
-    'Thirty',
-    'Forty',
-    'Fifty',
-    'Sixty',
-    'Seventy',
-    'Eighty',
-    'Ninety',
+    'Veinte',
+    'Treinta',
+    'Cuarenta',
+    'Cincuenta',
+    'Sesenta',
+    'Setenta',
+    'Ochenta',
+    'Noventa',
   ];
 
-  if (inputNumber === 0) return 'Zero';
+  if (inputNumber === 0) return 'Cero';
 
   // Recursive function to translate the number into words
   function translate(n: number) {
@@ -49,22 +49,19 @@ export function translateNumber(inputNumber: number): string {
       let rem = translate(n % 10);
       word = below_hundred[(n - (n % 10)) / 10 - 2] + ' ' + rem;
     } else if (n < 1000) {
-      word =
-        single_digit[Math.trunc(n / 100)] + ' Hundred ' + translate(n % 100);
+      word = single_digit[Math.trunc(n / 100)] + ' Cien ' + translate(n % 100);
     } else if (n < 1000000) {
       word =
-        translate(Math.floor(n / 1000)).trim() +
-        ' Thousand ' +
-        translate(n % 1000);
+        translate(Math.floor(n / 1000)).trim() + ' Mil ' + translate(n % 1000);
     } else if (n < 1000000000) {
       word =
         translate(Math.floor(n / 1000000)).trim() +
-        ' Million ' +
+        ' Millon ' +
         translate(n % 1000000);
     } else {
       word =
         translate(Math.floor(n / 1000000000)).trim() +
-        ' Billion ' +
+        ' Mil millones ' +
         translate(n % 1000000000);
     }
     return word;
